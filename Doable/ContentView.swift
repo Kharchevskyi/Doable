3bf77550c3a54525b5c833e05354b9068f77e6ee
Anton Kharchevskyi <kharchevskyi.a@gmail.com>
2020-03-05 09:54:13 +0100

    Initial commit

---
+++ Doable/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Doable
+//
+//  Created by Anton Kharchevskyi on 05/03/2020.
+//  Copyright © 2020 Anton Kharchevskyi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Doable!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
